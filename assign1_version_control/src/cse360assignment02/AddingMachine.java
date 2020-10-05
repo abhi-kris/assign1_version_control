be693a273cb842524968f3ff4f5630bc4bee6a38
Abhishek Krishnakumar <abhikris20@gmail.com>
2020-10-05 12:47:04 -0700

    First commit of Assign 1 Version Control

---
+++ assign1_version_control/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  
+  /**
+   * This is a constructor AddingMachine for this class. 
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  
+  /**
+   * This is used to get the total value
+   * @return 0 
+   */
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  
+  /**
+   * This is used to add values 
+   * @param value used to add 
+   */
+  
+  public void add (int value) {
+  }
+
+  /**
+   * This is used to subtract the values
+   * @param value used to subtract
+   */
+  
+  public void subtract (int value) {
+  }
+  
+  /**
+   * This is used to return a string object
+   */
+  
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * This is used to clear the memory. 
+   */
+  
+  public void clear() {
+  }
+}
